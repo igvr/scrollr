f57e323c3027007e8564110b893f40db67f34748
igvar <igvar@simulacrum.local>
2019-07-12 09:11:30 +0200

    Initial Commit

---
+++ aa/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  aa
+//
+//  Created by igvar on 12/07/2019.
+//  Copyright © 2019 igvar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
